3dd5c63673fc7842fea2dc28f132695b7bc82310
mi0254 <karan.uppal@myntra.com>
2018-06-04 15:57:52 +0530

    Partial code for duplicates in LL

---
+++ Linked List/remove-duplicates-linked-list.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct newNode(int num) {
+    int data;
+    struct newNode * next = NULL;
+};
+
+void printList (struct newNode * head) {
+    struct newNode * current = head;
+    while(current->next != NULL) {
+        cout<<current->data<<" ";
+        current = current->next;
+    }
+}
+
+int main() {
+
+    struct newNode * head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(4);
+    head->next->next->next = newNode(3);
+    head->next->next->next->next = newNode(4);
+    head->next->next->next->next->next = newNode(5);
+    head->next->next->next->next->next->next = newNode(2);
+    head->next->next->next->next->next->next->next = newNode(6);
+    head->next->next->next->next->next->next->next->next = newNode(4);
+
+    printList(head);
+
+    return 0;   
+}
