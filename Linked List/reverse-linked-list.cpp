55f746cfc315ab1cbb227a9371d7f4ac1cb4cd77
mi0254 <karan.uppal@myntra.com>
2018-06-15 21:25:46 +0530

    init the linked list

--- Linked List/reverse-linked-list.cpp
+++ Linked List/reverse-linked-list.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+struct node {
+    int data;
+    struct node * next;
+};
+
+struct node * newNode(int num) {
+    struct node * newNode = new node;
+    newNode->data = num;
+    newNode->next = NULL;
+    return newNode;
+}
+
 int main() {
+
+    struct node * head = newNode(1);
+    head->next = newNode(2);
+    head->next->next = newNode(3);
+    head->next->next->next = newNode(4);
+    
     return 0;
 }
