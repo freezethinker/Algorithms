aa4f78b2bb14abfb084f193fa13dd4d0d83fb901
mi0254 <karan.uppal@myntra.com>
2018-06-15 21:28:51 +0530

    functions addition

--- Linked List/reverse-linked-list.cpp
+++ Linked List/reverse-linked-list.cpp
@@ -14,12 +14,23 @@ struct node * newNode(int num) {
     return newNode;
 }
 
+void printLinkedList(struct node * head) {
+
+}
+
+void reverseLinkedList(struct node * head) {
+    
+}
+
 int main() {
 
     struct node * head = newNode(1);
     head->next = newNode(2);
     head->next->next = newNode(3);
     head->next->next->next = newNode(4);
-    
+
+    reverseLinkedList(head);
+    printLinkedList(head);
+
     return 0;
 }
