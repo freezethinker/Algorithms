6ccadd80522ea71cc5c7d027db14334fe1bbeca7
Karan Uppal <karan13aug@gmail.com>
2020-10-20 16:39:14 +0530

    handling cases when right branch is longer
    handling cases when right branch is longer

--- Trees/left-view.cpp
+++ Trees/left-view.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+/**
+ * Input :
+ *        1
+ *      /   \
+ *    2      3
+ *     \      \ 
+ *      4      7
+ *       \      \
+ *        5      8
+ *         \      \
+ *          6      9
+ *                  \
+ *                  10
+ * 
+ * Output :1 2 4 5 6 10
+**/
+
+int max_level = 0;
+
 struct node {
     int data;
     struct node * left;
@@ -15,12 +34,23 @@ struct node * newNode(int num) {
     return newNode;
 }
 
-void printLeftView(struct node * root) {
+void printLeftView(struct node * root, int current_level) {
     if(root == NULL) return;
+    
+    // cout<<"{META :: Element:" << root->data << " Current Level: " << current_level << " Max Level: " << max_level << "}" << endl;
+    
+    if(max_level < current_level) {
+        cout << root->data << " ";
+         max_level = current_level;
+    }
 
-    cout<<root->data<<endl;
-    if(root->left != NULL) printLeftView(root->left);
-    else printLeftView(root->right);
+    printLeftView(root->left, current_level + 1);
+    printLeftView(root->right, current_level + 1);
+}
+
+void leftView(struct node * root) {
+    if(root == NULL) return;
+    printLeftView(root, 1);
 }
 
 int main() {
@@ -30,14 +60,16 @@ int main() {
     root->left = newNode(2);
     root->right = newNode(3);
 
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
+    root->left->right = newNode(4);
+    root->left->right->right = newNode(5);
+    root->left->right->right->right = newNode(6);
+    
     root->right->right = newNode(7);
+    root->right->right->right = newNode(8);
+    root->right->right->right->right = newNode(9);
+    root->right->right->right->right->right = newNode(10);
 
-    root->left->left->right = newNode(8);
-
-    printLeftView(root);
+    leftView(root);
 
     return 0;
 }
